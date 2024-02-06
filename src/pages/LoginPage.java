b585cebfaed674b86ae51bab422eda2764e4bbf3
Ajay <kanakdande97@gmail.com>
2024-02-06 23:17:11 +0530

    Added basic infrastructure of framework

---
+++ src/pages/LoginPage.java
@@ -0,0 +1,15 @@
+package pages;
+
+public class LoginPage {
+	
+	public void loginToApp()
+	{
+		System.out.println("login to application");
+		
+	}
+	public void fetchingTitle ()
+	{
+		System.out.println("Getting title of page");
+		
+	}
+}
